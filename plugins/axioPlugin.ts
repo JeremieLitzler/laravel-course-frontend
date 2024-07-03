50b68a4c2b9eb2a57618064b8c5e5bba2a59e0ed
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-07-03 14:37:49 +0200

    resolve axios not setting header?

--- plugins/axioPlugin.ts
+++ plugins/axioPlugin.ts
@@ -1,24 +1,14 @@
 import axios from 'axios';
 
 export default defineNuxtPlugin(async (nuxtApp) => {
-  axios.defaults.baseURL = 'https://laravel-backend-vueschool.madebyjeremie.fr';
+  const config = useRuntimeConfig();
+
+  axios.defaults.baseURL = config.public.appURL;
   axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
   axios.defaults.headers.common['Content-Type'] = 'application/json';
   axios.defaults.headers.common['Accept'] = 'application/json';
   axios.defaults.withCredentials = true;
+  axios.defaults.withXSRFToken = true;
 
-  await axios.get('/sanctum/csrf-cookie');
-
-  // thanks to https://github.com/axios/axios/issues/6047#issuecomment-1786785122
-  axios.interceptors.request.use((config) => {
-    console.log('interceptors > request', document.cookie);
-    console.log('interceptors > request', document.cookie.split(';'));
-
-    const token = decodeURIComponent(
-      document.cookie.replace('XSRF-TOKEN=', '')
-    );
-    axios.defaults.headers['X-XSRF-TOKEN'] = token;
-
-    return config;
-  });
+  await axios.get('/sanctum/csrf-cookie', { baseURL: config.public.appURL });
 });
