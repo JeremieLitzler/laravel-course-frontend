17600c5cda209f0a2151602b75c93a9f36bde7b7
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-07-03 14:56:12 +0200

    resolve axios not setting header?

--- plugins/axioPlugin.ts
+++ plugins/axioPlugin.ts
@@ -7,8 +7,12 @@ export default defineNuxtPlugin(async (nuxtApp) => {
   axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
   axios.defaults.headers.common['Content-Type'] = 'application/json';
   axios.defaults.headers.common['Accept'] = 'application/json';
+  const xsrfToken = await axios.get<string>('/sanctum/csrf-cookie', {
+    baseURL: config.public.appURL,
+  });
+  console.log('xsrfToken', xsrfToken);
+
+  axios.defaults.headers.common['XSRF-TOKEN'] = xsrfToken.data;
   axios.defaults.withCredentials = true;
   axios.defaults.withXSRFToken = true;
-
-  await axios.get('/sanctum/csrf-cookie', { baseURL: config.public.appURL });
 });
